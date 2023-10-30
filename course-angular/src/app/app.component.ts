4c5ea8f2ee90cbc8e4ca22d71194b2a27b63f9c1
SebastianXPG <juanusugas12@gmail.com>
2023-10-30 10:44:30 -0500

    Esta es la primer parte del curso donde se inica aprendiendo que es el databinding , se aprende a utilizar  las direferentes maneras de usarse como la interpolacion , el event binding, el property binding y etc... , y por ultimo se aprende sobre las directivas como *ngFor , *ngIf etc...

--- course-angular/src/app/app.component.ts
+++ course-angular/src/app/app.component.ts
@@ -6,5 +6,40 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'course-angular';
+  // nombre = 'juan'
+  // textoPlaceholdfer = 'Escriba algo aquí'
+  // deshabilitado = true
+  // img = 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/cf/Angular_full_color_logo.svg/2048px-Angular_full_color_logo.svg.png'
+
+  // constructor() {
+  //   setInterval( ()=> this.deshabilitado = false ,3000);
+  // }
+
+  // getSuma(numero1: number, numero2: number) {
+  //   return numero1 + numero2;
+  // }
+
+
+  // texto = 'Esto es un video sobre event Binding'
+
+  // cambiarTexto(): void {
+  //   this.texto = 'En el proximo video vamos a ver Two way data-Binding'
+  // }
+
+  // texto = ' ';
+
+  listEstudiantes: any[] = [
+    { nombre: 'Thomas Gonzales', estado: 'Promocionado'},
+    { nombre: 'Crstian Gonzales', estado: 'Regular'},
+    { nombre: 'Patricia Funes', estado: 'Libre'},
+    { nombre: 'Sebas Gonzales', estado: 'Regular'},
+    { nombre: 'Marta Perez', estado: 'Promocionado'},
+    { nombre: 'Nicolas Gomez', estado: 'Libre'},
+  ]
+
+  mostrar = true
+
+  toogle(): void {
+    this.mostrar = !this.mostrar
+  }
 }
